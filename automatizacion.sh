d513b47360b794a8626fb54b87647465e1040f70
AimarUriarte <aimaruri01@gmail.com>
2023-06-15 00:02:31 +0200

    02:análisis estático realizado

---
+++ automatizacion.sh
@@ -0,0 +1,8 @@
+# Acccedemos a la carpeta del proyecto
+cd ./MifareClassicTool/Mifare\ Classic\ Tool/
+# Realizamos un análisis de código estático y guardamos el resultado en un fichero
+/home/alumno/pmd-bin-7.0.0-rc2/bin/pmd check -d . -R rulesets/java/quickstart.xml -f text > ../../Analisis/analisis_MifareClassicTool
+# Realizamos la compilación y el empaquetado para generar el entregable
+./gradlew assembleDebug
+#Movemos el entregable a la carpeta correspondiente de entregables
+cp ./app/build/outputs/apk/debug/app-debug.apk ../../Entregables/
